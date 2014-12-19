ade77edd36d5b9edcc4efa8edd70461157a5a5dd
Daniel Edler <edler.dev@gmail.com>
2014-12-19 16:32:47 +0100

    More automatic work with setup.py for compiling python module.

--- swig/setup.py
+++ swig/setup.py
@@ -1,39 +1,38 @@
 #!/usr/bin/env python
 
 """
-setup.py file for SWIG Infomap module
+setup.py file for compiling Infomap module
 """
 
 from distutils.core import setup, Extension
+import fnmatch
+import os
+import re
+
+cppSources = []
+for root, dirnames, filenames in os.walk('.'):
+    if root == 'src': cppSources.append(os.path.join(root, 'Infomap.cpp'))
+    else:
+        for filename in fnmatch.filter(filenames, '*.cpp'):
+            cppSources.append(os.path.join(root, filename))
+
+# Extract Infomap version
+infomapVersion = ''
+with open(os.path.join('src', 'io', 'version.cpp')) as f:
+    for line in f:
+        m = re.match( r'.+INFOMAP_VERSION = \"(.+)\"', line)
+        if m: infomapVersion = m.groups()[0]
 
 infomap_module = Extension('_infomap',
-	sources=['infomap_wrap.cpp',
-	'src/Infomap.cpp',
-	'src/infomap/FlowNetwork.cpp',
-	'src/infomap/InfomapBase.cpp',
-	'src/infomap/InfomapContext.cpp',
-	'src/infomap/MemFlowNetwork.cpp',
-	'src/infomap/MemNetwork.cpp',
-	'src/infomap/MultiplexNetwork.cpp',
-	'src/infomap/Network.cpp',
-	'src/infomap/NetworkAdapter.cpp',
-	'src/infomap/Node.cpp',
-	'src/infomap/TreeData.cpp',
-	'src/io/ClusterReader.cpp',
-	'src/io/HierarchicalNetwork.cpp',
-	'src/io/ProgramInterface.cpp',
-	'src/io/TreeDataWriter.cpp',
-	'src/io/version.cpp',
-	'src/utils/FileURI.cpp',
-	'src/utils/Logger.cpp'
-	],
-	extra_compile_args=['-DNO_MAIN']
-	)
+    sources=cppSources,
+    extra_compile_args=['-DNO_MAIN']
+    )
 
 setup (name = 'infomap',
-	version = '0.1',
-	author      = "SWIG Docs",
-	description = """Infomap clustering algorithm""",
-	ext_modules = [infomap_module],
-	py_modules = ["infomap"],
-	)
+    version = infomapVersion,
+    author      = "Team at mapequation.org",
+    description = """Infomap clustering algorithm""",
+    url         = "www.mapequation.org",
+    ext_modules = [infomap_module],
+    py_modules = ["infomap"],
+    )
