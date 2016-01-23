0ab2f15e1430fba65fe32b7a5d5e01042ebdb245
Daniel Edler <edler.dev@gmail.com>
2016-01-23 10:51:43 +0100

    Also put nodes not included in ordinary .tree in their own modules

--- src/infomap/NetworkAdapter.cpp
+++ src/infomap/NetworkAdapter.cpp
@@ -337,14 +337,18 @@ void NetworkAdapter::readHumanReadableTree(std::string filename)
 		++nodeCount;
 		maxDepth = std::max(maxDepth, depth);
 	}
-	if (nodeCount < m_numNodes)
-		throw MisMatchError("There are less nodes in the tree than in the network.");
 
 	if (!gotOriginalIndex)
 		throw FileFormatError("Not implemented for old .tree format (missing original index column).");
 
 	if (maxDepth < 2)
 		throw InputDomainError("No modular solution found in file.");
+
+	if (nodeCount < m_numNodes) {
+		// Add unassigned nodes to their own modules
+		unsigned int numUnassignedNodes = m_numNodes - nodeCount;
+		Log() << "\n -> Warning: " << numUnassignedNodes << " unassigned nodes are put in their own modules.";
+	}
 
 	if (numNodesNotFound > 0)
 		Log() << "\n -> Warning: " << numNodesNotFound << " nodes not found in network.";
@@ -357,6 +361,8 @@ void NetworkAdapter::readHumanReadableTree(std::string filename)
 
 	root->releaseChildren();
 
+	std::vector<unsigned int> assignedNodes(m_numNodes, 0);
+
 	// Replace externally loaded leaf nodes with network nodes
 	for (NodeBase::leaf_module_iterator leafModuleIt(m_treeData.root()); !leafModuleIt.isEnd(); ++leafModuleIt)
 	{
@@ -365,10 +371,22 @@ void NetworkAdapter::readHumanReadableTree(std::string filename)
 		for (NodeBase::sibling_iterator nodeIt(leafModuleIt->begin_child()); !nodeIt.isEnd(); ++nodeIt, ++childIndex)
 		{
 			leafNodes[childIndex] = &m_treeData.getLeafNode(nodeIt->originalIndex);
+			++assignedNodes[nodeIt->originalIndex];
 		}
 		leafModuleIt->deleteChildren();
 		for (childIndex = 0; childIndex < leafNodes.size(); ++childIndex)
 			leafModuleIt->addChild(leafNodes[childIndex]);
+	}
+
+	// Put unassigned nodes in their own modules
+	if (nodeCount < m_numNodes) {
+		for (unsigned int i = 0; i < m_numNodes; ++i) {
+			if (assignedNodes[i] == 0) {
+				NodeBase* module = m_treeData.nodeFactory().createNode("", 0.0, 0.0);
+				m_treeData.root()->addChild(module);
+				module->addChild(&m_treeData.getLeafNode(i));
+			}
+		}
 	}
 
 	Log() << "done! Found " << maxDepth << " levels." << std::endl;
