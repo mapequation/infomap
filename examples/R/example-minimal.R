6408c9d59b8a5c231cff12a0333278e0b1c487f6
Daniel Edler <edler.dev@gmail.com>
2018-02-08 19:19:55 +0100

    Update R examples with the Infomap wrapper

--- examples/R/example-minimal.R
+++ examples/R/example-minimal.R
@@ -1,34 +1,28 @@
 
 source("load-infomap.R")
 
-conf <- init("--two-level --silent")
+infomap <- Infomap("--two-level --silent")
 # Add output directory (and output name) to automatically write result to file
-# conf <- init("--two-level --silent . --out-name test")
+# infomap <- Infomap("--two-level . --out-name test")
 
-network <- Network(conf);
+infomap$addLink(0, 1);
+infomap$addLink(0, 2);
+infomap$addLink(0, 3);
+infomap$addLink(1, 0);
+infomap$addLink(1, 2);
+infomap$addLink(2, 1);
+infomap$addLink(2, 0);
+infomap$addLink(3, 0);
+infomap$addLink(3, 4);
+infomap$addLink(3, 5);
+infomap$addLink(4, 3);
+infomap$addLink(4, 5);
+infomap$addLink(5, 4);
+infomap$addLink(5, 3);
 
-network$addLink(0, 1);
-network$addLink(0, 2);
-network$addLink(0, 3);
-network$addLink(1, 0);
-network$addLink(1, 2);
-network$addLink(2, 1);
-network$addLink(2, 0);
-network$addLink(3, 0);
-network$addLink(3, 4);
-network$addLink(3, 5);
-network$addLink(4, 3);
-network$addLink(4, 5);
-network$addLink(5, 4);
-network$addLink(5, 3);
+infomap$run()
 
-network$finalizeAndCheckNetwork()
-
-cat("Created network with", network$numNodes(), "nodes and", network$numLinks(), "links.\n")
-
-tree <- HierarchicalNetwork(conf)
-
-run(network, tree);
+tree <- infomap$tree
 
 clusterIndexLevel <- 1 # 1, 2, ... or -1 for top, second, ... or lowest cluster level
 leafIt <- tree$leafIter(clusterIndexLevel)
