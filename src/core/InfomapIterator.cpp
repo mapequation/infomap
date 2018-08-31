777dd274e115138473c494fbfbe546f847895278
Anton Eriksson <anton.eriksson@umu.se>
2018-08-31 10:14:40 +0200

    Infomap iterator refactor (#27)
    Some code cleaning, including replacing goto with while

--- src/core/InfomapIterator.cpp
+++ src/core/InfomapIterator.cpp
@@ -7,67 +7,65 @@ namespace infomap {
 
 	InfomapIterator& InfomapIterator::operator++()
 	{
-		InfoNode* curr = m_current;
-		InfoNode* infomapRoot = curr->getInfomapRoot();
-		if (infomapRoot != nullptr)
-		{
-			curr = infomapRoot;
-		}
+		const auto root = m_current->getInfomapRoot();
+		auto current = root ? root : m_current;
 
-		if(curr->firstChild != nullptr)
-		{
-			curr = curr->firstChild;
+		if (current->firstChild) {
+			current = current->firstChild;
 			++m_depth;
 			m_path.push_back(0);
-		}
-		else
-		{
+		} else {
 			// Current node is a leaf
 			// Presupposes that the next pointer can't reach out from the current parent.
-			tryNext:
-			while(curr->next == nullptr)
-			{
-				if (curr->parent != nullptr)
-				{
-					curr = curr->parent;
-					--m_depth;
-					m_path.pop_back();
-					if(curr == m_root) // Check if back to beginning
-					{
-						m_current = nullptr;
-						return *this;
-					}
-					if (m_moduleIndexLevel < 0) {
-						 if (curr->isLeafModule()) // TODO: Generalize to -2 for second level to bottom
-							 ++m_moduleIndex;
-					}
-					else if (static_cast<unsigned int>(m_moduleIndexLevel) == m_depth)
-						++m_moduleIndex;
-				}
-				else
-				{
-					InfoNode* infomapOwner = curr->owner;
-					if (infomapOwner != nullptr)
-					{
-						curr = infomapOwner;
-						if(curr == m_root) // Check if back to beginning
-						{
+
+			bool tryNext = true;
+
+			while (tryNext) {
+				tryNext = false;
+
+				while (!current->next) {
+					if (current->parent) {
+						current = current->parent;
+						--m_depth;
+						m_path.pop_back();
+
+						if (current == m_root) { // Check if back to beginning
 							m_current = nullptr;
 							return *this;
 						}
-						goto tryNext;
-					}
-					else // null also if no children in first place
-					{
-						m_current = nullptr;
-						return *this;
+
+						if (m_moduleIndexLevel < 0) {
+							if (current->isLeafModule()) { // TODO: Generalize to -2 for second level to bottom
+								++m_moduleIndex;
+							}
+						} else if (static_cast<unsigned int>(m_moduleIndexLevel) == m_depth) {
+							++m_moduleIndex;
+						}
+					} else {
+						if (current->owner) {
+							current = current->owner;
+
+							if (current == m_root) { // Check if back to beginning
+								m_current = nullptr;
+								return *this;
+							}
+
+							tryNext = true;
+							break;
+
+						} else { // null also if no children in first place
+							m_current = nullptr;
+							return *this;
+						}
 					}
 				}
 			}
-			curr = curr->next;
+
+			current = current->next;
 			++m_path.back();
 		}
-		m_current = curr;
+
+		m_current = current;
 		return *this;
 	}
 
