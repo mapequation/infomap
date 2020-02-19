4d23bbda6423b2342f1f39e20875902625421c77
Anton Eriksson <anton.eriksson@umu.se>
2020-02-19 21:00:55 +0100

    Export Infomap version

--- interfaces/js/src/index.js
+++ interfaces/js/src/index.js
@@ -1,7 +1,10 @@
 import commits from "./commits.json";
+import { version } from "../../../package.json";
 import Infomap from "./worker.js";
+
 
 export {
   Infomap as default,
+  version as infomapVersion,
   commits as infomapChangelog
 };
