31a7e9446d1f9347b5205ec50988f7f433ab189b
Anton Eriksson <anton.eriksson@umu.se>
2020-02-19 15:28:53 +0100

    build: Add get-commits.js

---
+++ utils/get-commits.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+"use strict";
+
+const fsPromises = require("fs").promises;
+const gitRawCommits = require("git-raw-commits");
+const conventionalCommitsParser = require("conventional-commits-parser");
+
+function getCommits(from = "", to = "HEAD") {
+  const commits = [];
+
+  const gitOpts = { from, to };
+
+  return new Promise((resolve, reject) =>
+    gitRawCommits(gitOpts)
+      .pipe(conventionalCommitsParser())
+      .on("data", (data) => commits.push(data))
+      .on("finish", () => resolve(commits))
+      .on("error", reject));
+}
+
+function getCommitWriter(filename) {
+  return commits => fsPromises.writeFile(filename, JSON.stringify(commits, null, 2));
+}
+
+module.exports = { getCommits, getCommitWriter };
