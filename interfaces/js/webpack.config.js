0db5432fd09a3b17f5252442070c69322882f300
Anton Eriksson <anton.eriksson@umu.se>
2020-02-19 22:23:09 +0100

    Use webpack.DefinePlugin to create build constants

--- interfaces/js/webpack.config.js
+++ interfaces/js/webpack.config.js
@@ -1,31 +1,47 @@
 const path = require("path");
+const DefinePlugin = require("webpack").DefinePlugin;
+const { getCommits } = require("../../utils/get-commits.js");
+const { version } = require("../../package.json");
 
-module.exports = {
-  mode: "production",
-  entry: "./interfaces/js/src/index.js",
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./interfaces/js/dist",
-  },
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-    library: "infomap",
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-class-properties"]
+
+const webpackConfig = async () => {
+  const commits = await getCommits("2d94e92");
+
+  return {
+    mode: "production",
+    entry: "./interfaces/js/src/index.js",
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: "./interfaces/js/dist",
+    },
+    output: {
+      filename: "index.js",
+      path: path.resolve(__dirname, "dist"),
+      library: "infomap",
+      libraryTarget: "umd",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-class-properties"]
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+    plugins: [
+      new DefinePlugin({
+        CHANGELOG: JSON.stringify(commits, null, 2),
+        VERSION: version,
+      })
+    ]
+  }
 };
+
+module.exports = webpackConfig
