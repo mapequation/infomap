5f1e2e90c874f806429c726032b7cbdce6aee449
Anton Eriksson <anton.eriksson@umu.se>
2020-02-26 16:34:50 +0100

    Fix typo

--- interfaces/js/src/index.js
+++ interfaces/js/src/index.js
@@ -2,7 +2,7 @@ import Infomap from "./infomap.js";
 
 const infomapChangelog = CHANGELOG;
 const infomapParameters = PARAMETERS;
-const exampleNetworks = NETWORKS;
+const exampleNetworks = EXAMPLE_NETWORKS;
 
 export {
   Infomap as default,
