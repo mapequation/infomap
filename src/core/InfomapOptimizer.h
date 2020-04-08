d96734505b61d5565215d293788dc6daf167726f
Anton Eriksson <anton.eriksson@umu.se>
2020-04-08 18:45:16 +0200

    Don't shadow variable in the same scope

--- src/core/InfomapOptimizer.h
+++ src/core/InfomapOptimizer.h
@@ -960,7 +960,7 @@ unsigned int InfomapOptimizer<Objective>::tryMoveEachNodeIntoBestModuleInParalle
 
 					// Recalculate delta codelength for proposed move to see if still an improvement
 
-					DeltaFlowDataType oldModuleDelta(oldModuleIndex, 0.0, 0.0);
+					oldModuleDelta = DeltaFlowDataType(oldModuleIndex, 0.0, 0.0);
 					DeltaFlowDataType newModuleDelta(bestModuleIndex, 0.0, 0.0);
 
 
