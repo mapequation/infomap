5c150e2a696da89e75feaecab12d93de2ab9aa9b
Anton Eriksson <anton.eriksson@umu.se>
2020-04-08 18:41:51 +0200

    Mark unused variable

--- src/core/InfomapOptimizer.h
+++ src/core/InfomapOptimizer.h
@@ -176,7 +176,7 @@ double InfomapOptimizer<MetaMapEquation>::getMetaCodelength(bool unweighted) con
 
 template<typename Objective>
 inline
-double InfomapOptimizer<Objective>::getMetaCodelength(bool unweighted) const
+double InfomapOptimizer<Objective>::getMetaCodelength(/* unused */ bool unweighted) const
 {
 	return 0.0;
 }
