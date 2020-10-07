4f9ef88409012ff00457291fc0ad8d0f33063c59
Anton Eriksson <anton.eriksson@umu.se>
2020-10-07 08:33:44 +0200

    fix(windows): Include algorithm header
    Closes #150

--- src/io/ClusterReader.cpp
+++ src/io/ClusterReader.cpp
@@ -25,6 +25,7 @@
 **********************************************************************************/
 
 
+#include <algorithm>
 #include "ClusterReader.h"
 #include "../utils/Log.h"
 #include "convert.h"
