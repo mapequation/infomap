8500f8ab117299a0d447fb8baf46462101d76bb2
Anton Eriksson <anton.eriksson@umu.se>
2021-06-08 17:49:19 +0200

    docs(python): Add sphinx.ext.githubpages to simplify Makefile

--- interfaces/python/source/conf.py
+++ interfaces/python/source/conf.py
@@ -36,6 +36,7 @@ with open('../../../package.json', 'r') as fp:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.githubpages',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon'
 ]
@@ -59,7 +60,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 napoleon_numpy_docstring = True
 napoleon_google_docstring = False
