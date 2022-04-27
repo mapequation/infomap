2d75281ad477281f977c26f8273eb54897c6773d
Anton Eriksson <anton.eriksson@umu.se>
2022-04-27 13:35:37 +0200

    chore: Add missing copyright notices

--- src/core/MapEquation.cpp
+++ src/core/MapEquation.cpp
@@ -1,9 +1,26 @@
-/*
- * MapEquation.h
- *
- *  Created on: 25 feb 2015
- *      Author: Daniel
- */
+/*******************************************************************************
+  Infomap software package for multi-level network clustering
+
+  Copyright (c) 2013, 2014 Daniel Edler, Martin Rosvall
+
+  For more information, see <http://www.mapequation.org>
+
+  This file is part of Infomap software package.
+
+  Infomap software package is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Infomap software package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with Infomap software package.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 #include "MapEquation.h"
 #include "../utils/infomath.h"
 #include "../utils/convert.h"
@@ -12,6 +29,7 @@
 #include "../utils/VectorMap.h"
 #include "InfoNode.h"
 #include "FlowData.h"
+
 #include <vector>
 #include <map>
 #include <iostream>
