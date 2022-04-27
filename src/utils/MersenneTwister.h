6b74add1e79088b9e5cd58aba23475f4a7560229
Anton Eriksson <anton.eriksson@umu.se>
2022-04-27 13:35:37 +0200

    chore: Add missing copyright notices

--- src/utils/MersenneTwister.h
+++ src/utils/MersenneTwister.h
@@ -15,31 +15,6 @@
 // Equidistributed Uniform Pseudo-Random Number Generator", ACM Transactions on
 // Modeling and Computer Simulation, Vol. 8, No. 1, January 1998, pp 3-30.
 
-// Copyright (C) 2002  Richard J. Wagner
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-// The original code included the following notice:
-//
-//     Copyright (C) 1997, 1999 Makoto Matsumoto and Takuji Nishimura.
-//     When you use this, send an email to: matumoto@math.keio.ac.jp
-//     with an appropriate reference to your work.
-//
-// It would be nice to CC: rjwagner@writeme.com and Cokus@math.washington.edu
-// when you write.
-
 #ifndef MERSENNETWISTER_H
 #define MERSENNETWISTER_H
 
