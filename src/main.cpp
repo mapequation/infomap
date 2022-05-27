mapequation/infomap

commit 53d6092a4ddc838759d2d99f64e7dcdeca2404ca

refactor: Don't use Python wrapper class when instantiating Infomap

src/main.cpp
@@ -7,8 +7,8 @@
  For more information, see <http://www.mapequation.org>
  ******************************************************************************/
 
+#include "core/InfomapCore.h"
 #include "io/Config.h"
-#include "Infomap.h"
 #include <iostream>
 #include <stdexcept>
 
@@ -23,7 +23,7 @@ int run(const std::string& flags)
   try {
     Config conf(flags, true);
 
-    InfomapWrapper infomap(conf);
+    InfomapCore infomap(conf);
 
     infomap.run();
   } catch (std::exception& e) {

commit 78e80c581edd92001818abdf50066b798251c604

fix: Json output causes segfault
Introduced in 53d6092

src/main.cpp
@@ -7,7 +7,7 @@
  For more information, see <http://www.mapequation.org>
  ******************************************************************************/
 
-#include "core/InfomapBase.h"
+#include "Infomap.h"
 #include <iostream>
 #include <stdexcept>
 
@@ -20,7 +20,7 @@ namespace infomap {
 int run(const std::string& flags)
 {
   try {
-    InfomapBase(flags, true).run();
+    InfomapWrapper(Config(flags, true)).run();
   } catch (std::exception& e) {
     std::cerr << "Error: " << e.what() << '\n';
     return 1;
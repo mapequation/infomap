78e80c581edd92001818abdf50066b798251c604
Anton Eriksson <anton.eriksson@umu.se>
2022-05-27 15:47:20 +0200

    fix: Json output causes segfault
    Introduced in 53d6092

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
  For more information, see <http://www.mapequation.org>
  ******************************************************************************/
 
-#include "core/InfomapBase.h"
+#include "Infomap.h"
 #include <iostream>
 #include <stdexcept>
 
@@ -20,7 +20,7 @@ namespace infomap {
 int run(const std::string& flags)
 {
   try {
-    InfomapBase(flags, true).run();
+    InfomapWrapper(Config(flags, true)).run();
   } catch (std::exception& e) {
     std::cerr << "Error: " << e.what() << '\n';
     return 1;
