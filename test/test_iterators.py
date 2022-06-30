7fc4ce3038f560f3177ffb400449135e84497f6b
Daniel Edler <edler.dev@gmail.com>
2022-06-30 20:53:51 +0200

    wip(python): Add iterator tests

--- test/test_iterators.py
+++ test/test_iterators.py
@@ -29,6 +29,20 @@ def test_iter_physical_reliability():
         modules = [(node.node_id, node.module_id) for node in im.physical_nodes]
         assert modules == [(1, 1), (2, 1), (3, 1), (1, 2), (4, 2), (5, 2)]
 
+def test_multilevel_modules_on_states():
+    im = Infomap(silent=True)
+    im.read_file("examples/networks/states.net")
+    im.run()
+    modules = [(node, modules) for node, modules in im.get_multilevel_modules(states=True).items()]
+    assert modules == [(1, (1,)), (2, (1,)), (3, (1,)), (4, (2,)), (5, (2,)), (6, (2,))]
+
+def test_multilevel_modules_on_physical():
+    im = Infomap(silent=True)
+    im.read_file("examples/networks/states.net")
+    im.run()
+    # todo: Use unit test function to assert exception is raised
+    # RuntimeError: Cannot get multilevel modules on higher-order network without states.
+    modules = im.get_multilevel_modules(states=False)
 
 
 
@@ -36,3 +50,4 @@ if __name__ == "__main__":
     test_iter_physical_on_physical()
     test_iter_physical_on_states()
     test_iter_physical_reliability()
+    test_multilevel_modules_on_states()
