223adcc753344a61c0d73db49576dd8f95ed588d
Daniel Edler <edler.dev@gmail.com>
2024-06-20 18:54:18 +0200

    docs(py): Add state and multilayer examples for networkx

--- examples/python/infomap-networkx.py
+++ examples/python/infomap-networkx.py
@@ -76,3 +76,48 @@ print(f"Found {im.num_top_modules} modules with codelength {im.codelength:.8f} b
 nx.set_node_attributes(G, im.get_modules(), "community")
 
 draw_network(G)
+
+print("A state network...")
+G = nx.Graph()
+G.add_node("a", phys_id=1)
+G.add_node("b", phys_id=2)
+G.add_node("c", phys_id=3)
+G.add_node("d", phys_id=1)
+G.add_node("e", phys_id=4)
+G.add_node("f", phys_id=5)
+G.add_edge("a", "b")
+G.add_edge("a", "c")
+G.add_edge("b", "c")
+G.add_edge("d", "e")
+G.add_edge("d", "f")
+G.add_edge("e", "f")
+im = Infomap(silent=True)
+im.add_networkx_graph(G)
+im.run()
+for node in im.nodes:
+    print(node.node_id, node.module_id, node.flow, node.state_id)
+# 1 1 0.16666666666666666 0
+# 2 1 0.16666666666666666 1
+# 3 1 0.16666666666666666 2
+# 1 2 0.16666666666666666 3
+# 4 2 0.16666666666666666 4
+# 5 2 0.16666666666666666 5
+
+
+print("A multilayer network...")
+G = nx.Graph()
+G.add_node(11, phys_id=1, layer_id=1)
+G.add_node(21, phys_id=2, layer_id=1)
+G.add_node(22, phys_id=2, layer_id=2)
+G.add_node(23, phys_id=3, layer_id=2)
+G.add_edge(11, 21)
+G.add_edge(22, 23)
+im = Infomap(silent=True)
+im.add_networkx_graph(G)
+im.run()
+for node in im.nodes:
+    print(node.node_id, node.module_id, node.flow, node.state_id, node.layer_id)
+# 1 1 0.25 0 1
+# 2 1 0.25 1 1
+# 2 2 0.25 2 2
+# 3 2 0.25 3 2
