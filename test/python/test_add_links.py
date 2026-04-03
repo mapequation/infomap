3df4d44868f86a447f99486d86f5919b0dab91b7
Anton Holmgren <antoneri@gmail.com>
2026-04-03 10:45:10 +0200

    test(shared): move core regressions to cpp fixtures

--- test/python/test_add_links.py
+++ test/python/test_add_links.py
@@ -4,59 +4,21 @@ import pytest
 pytestmark = pytest.mark.fast
 
 
-def test_add_link(make_infomap):
+def test_add_link_smoke(make_infomap, load_graph_fixture, canonical_modules):
     im = make_infomap()
-    links = [
-        [1, 2],
-        [2, 3],
-        [3, 1],
-        [3, 4],
-        [4, 5],
-        [5, 6],
-        [6, 4],
-    ]
-
-    for link in links:
-        im.add_link(*link)
+    load_graph_fixture(im, "twotriangles_unweighted.edges", method="add_link")
 
     im.run()
 
     assert im.num_top_modules == 2
+    assert canonical_modules(im.get_modules()) == [[1, 2, 3], [4, 5, 6]]
 
 
-def test_add_links_unweighted(make_infomap):
+def test_add_links_smoke(make_infomap, load_graph_fixture, canonical_modules):
     im = make_infomap()
-    links = [
-        [1, 2],
-        [2, 3],
-        [3, 1],
-        [3, 4],
-        [4, 5],
-        [5, 6],
-        [6, 4],
-    ]
-
-    im.add_links(links)
+    load_graph_fixture(im, "twotriangles_weighted.edges", method="add_links")
 
     im.run()
 
     assert im.num_top_modules == 2
-
-
-def test_add_links_weighted(make_infomap):
-    im = make_infomap()
-    links = [
-        [1, 2, 0.5],
-        [2, 3, 0.5],
-        [3, 1, 0.5],
-        [3, 4, 0.5],
-        [4, 5, 0.5],
-        [5, 6, 0.5],
-        [6, 4, 0.5],
-    ]
-
-    im.add_links(links)
-
-    im.run()
-
-    assert im.num_top_modules == 2
+    assert canonical_modules(im.get_modules()) == [[1, 2, 3], [4, 5, 6]]
