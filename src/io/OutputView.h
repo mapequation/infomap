b38ddb2a7532f3c123e70fa9a8bc4fc5d8671ee0
Anton Holmgren <antoneri@gmail.com>
2026-05-20 22:47:04 +0200

    refactor: output artifact planning (#575)

---
+++ src/io/OutputView.h
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ Infomap software package for multi-level network clustering
+ Copyright (c) 2013, 2014 Daniel Edler, Anton Holmgren, Martin Rosvall
+
+ This file is part of the Infomap software package.
+ See file LICENSE_GPLv3.txt for full license details.
+ For more information, see <http://www.mapequation.org>
+ ******************************************************************************/
+
+#ifndef OUTPUT_VIEW_H_
+#define OUTPUT_VIEW_H_
+
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <map>
+#include <string>
+
+namespace infomap {
+
+class InfomapBase;
+class InfoNode;
+class StateNetwork;
+
+enum class OutputLeafPolicy : std::uint8_t {
+  HideBipartite,
+  HideBipartiteUnlessFlowTree
+};
+
+struct OutputLeafRow {
+  const InfoNode& node;
+  std::deque<unsigned int> path;
+  unsigned int moduleId = 0;
+  double modularCentrality = 0.0;
+  unsigned int stateId = 0;
+  unsigned int physicalId = 0;
+  unsigned int layerId = 0;
+  double flow = 0.0;
+  std::string name;
+};
+
+struct OutputTreeRow {
+  const InfoNode& node;
+  unsigned int depth = 0;
+  unsigned int stateId = 0;
+  unsigned int physicalId = 0;
+  double flow = 0.0;
+};
+
+struct OutputStateNodeTarget {
+  InfoNode* parent = nullptr;
+  unsigned int childIndex = 0;
+};
+
+class OutputView {
+public:
+  using LeafCallback = std::function<void(const OutputLeafRow&)>;
+  using TreeCallback = std::function<void(const OutputTreeRow&)>;
+
+  OutputView(InfomapBase& infomap, const StateNetwork& network, bool states);
+
+  bool isStateLevel() const { return m_states; }
+  bool isPhysicalLevel() const { return !m_states; }
+  bool isHigherOrderPhysicalLevel() const;
+  bool isMultilayer() const;
+  bool hasMetaData() const;
+
+  void forEachLeaf(int moduleIndexLevel, OutputLeafPolicy filter, const LeafCallback& callback);
+  void forEachTreeNode(const TreeCallback& callback);
+
+  std::map<unsigned int, OutputStateNodeTarget> stateNodeTargets();
+
+private:
+  bool shouldIncludeLeaf(const InfoNode& node, OutputLeafPolicy filter) const;
+  OutputLeafRow leafRow(const InfoNode& node,
+                        const std::deque<unsigned int>& path,
+                        unsigned int moduleId,
+                        double modularCentrality) const;
+  OutputTreeRow treeRow(const InfoNode& node, unsigned int depth) const;
+  std::string nodeName(const InfoNode& node) const;
+
+  InfomapBase& m_infomap;
+  const StateNetwork& m_network;
+  bool m_states = false;
+};
+
+} // namespace infomap
+
+#endif // OUTPUT_VIEW_H_
--- test/cpp/test_config.cpp
+++ test/cpp/test_config.cpp
@@ -3,6 +3,7 @@
 #include "io/ConfigBuilder.h"
 #include "io/Config.h"
 #include "io/OutputFormats.h"
+#include "io/OutputPlan.h"
 #include "io/ParameterCatalog.h"
 
 #include <map>
@@ -11,6 +12,16 @@
 namespace {
 
 using infomap::Config;
+
+std::vector<std::string> resultKeysFor(const Config& config, infomap::OutputPhase phase)
+{
+  std::vector<std::string> keys;
+  for (const auto& artifact : infomap::planOutputArtifacts(config, phase)) {
+    keys.push_back(artifact.resultKey);
+  }
+  return keys;
+}
+
 TEST_CASE("Config accepts no-file-output without an output directory [fast][core][config][cli]")
 {
   CHECK_NOTHROW(Config("input.net --silent --no-file-output", true));
@@ -142,6 +153,81 @@ TEST_CASE("Output format manifest result keys stay unique [fast][core][config][c
   }
   CHECK(resultKeys.count("json_states") == 1);
   CHECK(resultKeys.count("flow_as_physical") == 1);
+}
+
+TEST_CASE("Output plan expands first-order modular artifacts [fast][core][config][output]")
+{
+  Config config;
+  config.outName = "network";
+  config.printTree = true;
+
+  const auto artifacts = infomap::planOutputArtifacts(config, infomap::OutputPhase::AfterPartition);
+
+  REQUIRE(artifacts.size() == 1);
+  CHECK(artifacts.front().resultKey == "tree");
+  CHECK(artifacts.front().label == "tree");
+  CHECK(artifacts.front().filename == "network.tree");
+  CHECK(artifacts.front().phase == infomap::OutputPhase::AfterPartition);
+  CHECK(artifacts.front().kind == infomap::OutputKind::Tree);
+  CHECK_FALSE(artifacts.front().states);
+}
+
+TEST_CASE("Output plan expands state and physical modular artifacts [fast][core][config][output]")
+{
+  Config config;
+  config.outName = "network";
+  config.printTree = true;
+  config.setStateOutput();
+
+  const auto artifacts = infomap::planOutputArtifacts(config, infomap::OutputPhase::AfterPartition);
+
+  REQUIRE(artifacts.size() == 2);
+  CHECK(resultKeysFor(config, infomap::OutputPhase::AfterPartition) == std::vector<std::string> { "tree", "tree_states" });
+  CHECK(artifacts[0].label == "physical tree");
+  CHECK(artifacts[0].filename == "network.tree");
+  CHECK_FALSE(artifacts[0].states);
+  CHECK(artifacts[1].label == "state tree");
+  CHECK(artifacts[1].filename == "network_states.tree");
+  CHECK(artifacts[1].states);
+}
+
+TEST_CASE("Output plan assigns network artifacts to write phases [fast][core][config][output]")
+{
+  Config config;
+  config.outName = "network";
+  config.printClu = true;
+  config.printTree = true;
+  config.printFlowNetwork = true;
+  config.printPajekNetwork = true;
+  config.printStateNetwork = true;
+  config.setStateOutput();
+
+  CHECK(resultKeysFor(config, infomap::OutputPhase::BeforeFlow) == std::vector<std::string> { "states", "states_as_physical" });
+  CHECK(resultKeysFor(config, infomap::OutputPhase::AfterFlow) == std::vector<std::string> { "flow_as_physical" });
+  CHECK(resultKeysFor(config, infomap::OutputPhase::AfterPartition) == std::vector<std::string> { "tree", "tree_states", "clu", "clu_states" });
+
+  for (const auto phase : { infomap::OutputPhase::BeforeFlow, infomap::OutputPhase::AfterFlow, infomap::OutputPhase::AfterPartition }) {
+    for (const auto& artifact : infomap::planOutputArtifacts(config, phase)) {
+      CHECK(artifact.phase == phase);
+      CHECK(infomap::findOutputFileFormat(artifact.resultKey) != nullptr);
+    }
+  }
+}
+
+TEST_CASE("Output plan applies trial suffix before format suffix [fast][core][config][output]")
+{
+  Config config;
+  config.outDirectory = "out/";
+  config.outName = "network";
+  config.printTree = true;
+  config.printAllTrials = true;
+  config.numTrials = 3;
+
+  const auto artifacts = infomap::planOutputArtifacts(config, infomap::OutputPhase::AfterPartition, 2);
+
+  REQUIRE(artifacts.size() == 1);
+  CHECK(infomap::outputPlanBasename(config, 2) == "out/network_trial_2");
+  CHECK(artifacts.front().filename == "out/network_trial_2.tree");
 }
 
 TEST_CASE("Parameter catalog owns option choices and binding names [fast][core][config][cli]")
